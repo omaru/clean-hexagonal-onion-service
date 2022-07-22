5c61c6e82bb34c41605dd2e97e8f3f6bba5b79d2
Maik Kingma <maik.kingma@codecentric.nl>
2022-07-22 10:36:53 +0200

    Initial Setup

---
+++ src/main/java/nl/codecentric/clean_hexagonal_onion_service/CleanHexagonalOnionServiceApplication.java
@@ -0,0 +1,13 @@
+package nl.codecentric.clean_hexagonal_onion_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CleanHexagonalOnionServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CleanHexagonalOnionServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/nl/codecentric/clean_hexagonal_onion_service/CleanHexagonalOnionServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package nl.codecentric.clean_hexagonal_onion_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CleanHexagonalOnionServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
